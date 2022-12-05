6ed00176b33b5e99219897e269332420c4cb53c8
davidmunoz626 <115696661+davidmunoz626@users.noreply.github.com>
2022-12-05 15:15:20 +0100

    newproperty

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom"
-import NewCoasterPage from "../pages/NewCoasterPage/NewCoasterPage";
 import SignUp from "../pages/SignUp/SignUp"
 import LogIn from "../pages/LogIn/LogIn"
 import HomePage from "../pages/HomePage/HomePage";
 import PropertiesListPage from "../pages/PropertiesListPage/PropertiesListPage";
+import NewPropertyPage from "../pages/NewPropertyPage/NewPropertyPage";
 
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/create-propety" element={<NewCoasterPage />} />
+            <Route path="/create-propety" element={<NewPropertyPage />} />
             <Route path="/properties" element={<PropertiesListPage />} />
             <Route path="/detalles/:houses_id" element={<p>detalles casa</p>} />
             <Route path="/hotels" element={<p>hoteles</p>} />
